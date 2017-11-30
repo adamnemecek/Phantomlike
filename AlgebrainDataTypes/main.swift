37f72966fa05541f14a324aa242d1b282727449c
Adam Nemecek <adamnemecek@gmail.com>
2017-11-29 17:27:23 -0800

    Initial Commit

---
+++ AlgebrainDataTypes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgebrainDataTypes
+//
+//  Created by Adam Nemecek on 11/29/17.
+//  Copyright © 2017 Adam Nemecek. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
